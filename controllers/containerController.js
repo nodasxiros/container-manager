08d9930efbc90b76a1c6e8b649a34a74303e804c
Nondas Xiros <nxiros@webjar.me>
2019-07-12 12:10:48 +0300

    Finished create method in containers controller

--- controllers/containerController.js
+++ controllers/containerController.js
@@ -4,10 +4,8 @@ const Docker = require('dockerode')
 let docker = new Docker({ socketPath: '/var/run/docker.sock' })
 
 exports.index = async (req, res) => {
-  const containers = await docker.listContainers()
-    .catch(er => {
-      res.send(er)
-    })
+  const containers = await docker.listContainers({ all: true })
+
   res.send(JSON.stringify(containers))
 }
 
@@ -38,19 +36,52 @@ exports.stop = async (req, res) => {
 }
 
 exports.create = async (req, res) => {
-  docker.createContainer({
-    Image: 'redis',
-    AttachStdin: false,
-    AttachStdout: true,
-    AttachStderr: true,
-    Tty: true,
-    Cmd: ['/bin/bash', '-c', 'tail -f /var/log/dmesg'],
-    OpenStdin: false,
-    StdinOnce: false
-  })
-    .then(container => {
-      container.start()
-      res.send(`Container ${container.id} has started`)
+  console.log(1)
+  await docker.pull('redis:latest')
+  console.log(2)
+  // const container = await docker.createContainer({
+  //   Image: 'redis',
+  //   AttachStdin: false,
+  //   AttachStdout: true,
+  //   AttachStderr: true,
+  //   Tty: true,
+  //   Cmd: ['/bin/bash', '-c', 'tail -f /var/log/dmesg'],
+  //   OpenStdin: false,
+  //   StdinOnce: false
+  // })
+
+  docker.buildImage('./redis.tar', { t: 'redis-three' }, (err, stream) => {
+    console.log(3)
+    if (err) return res.send(err)
+    console.log(4)
+    stream.pipe(process.stdout, {
+      end: true
     })
-    .catch(err => console.log(err))
+    console.log(5)
+
+    stream.on('end', function () {
+      docker.createContainer({
+        Image: 'redis'
+      }, function (err, container) {
+        if (err) return res.send(err)
+        container.attach({
+          stream: true,
+          stdout: true,
+          stderr: true,
+          tty: true
+        }, function (err, stream) {
+          if (err) return res.send(err)
+          stream.pipe(process.stdout)
+          container.start(function (err, data) {
+            if (err) return res.send(err)
+          })
+        })
+      })
+    })
+  })
+  //   .then(container => {
+  //     container.start()
+  //     res.send(`Container ${container.id} has started`)
+  //   })
+  //   .catch(err => console.log(err))
 }
