114845f62d78576ab08a0d23ec886d845079372b
Nondas Xiros <nxiros@webjar.me>
2019-07-15 00:22:35 +0300

    Created modules for getting container objects

---
+++ front/modules/containersModule.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const qs = require('qs')
+const settings = require('~/modules/settings.js')
+const { apiUrl } = settings
+
+var JSON = require('babel-runtime/core-js/json/stringify')
+
+axios.create({
+  crossorigin: true,
+  baseURL: apiUrl,
+  timeout: 1000,
+  headers: {
+    crossorigin: true
+  }
+})
+const containersObj = {
+  nuxt: function () {
+    if (typeof $nuxt !== 'undefined') {
+      return $nuxt
+    } else {
+      return null
+    }
+  },
+  get: function (id) {
+    if (typeof id == 'undefined') return axios.get(`${apiUrl}/containers`, {
+      crossdomain: true
+    })
+    return axios.get(`${apiUrl}/containers/${id}`)
+  },
+
+}
+
+module.exports = containersObj
---
+++ front/modules/settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+  appUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3035'
+}
